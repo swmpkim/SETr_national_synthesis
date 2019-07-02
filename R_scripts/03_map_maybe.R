aeea86da19594c1bc877c1ebb04461f1f26abce8
swmpkim <kimberly.cressman@dmr.ms.gov>
2019-07-02 15:50:18 -0500

    changed color palette; improved outlines and resolution of pie charts

--- R_scripts/03_map_maybe.R
+++ R_scripts/03_map_maybe.R
@@ -4,77 +4,28 @@ library(leaflet)
 
 # read in the points for the map
 in_path <- here::here("data", "intermediate", "locations.csv")
-loc_dat <- read.csv(in_path, stringsAsFactors = FALSE)
+loc_dat <- read.csv(in_path, stringsAsFactors = FALSE) %>% 
+        arrange(reserve) # make sure it's alphabetical because file lists for icons will be
 
 
-# read in the pie charts and turn them into icons
-in_path <- here::here("R_output", "figures", "pie_charts")
+# look for pie charts here
+in_path <- here::here("R_output", "figures", "for_maps")
 
-# file names will look like this
-files_0 <- dir(in_path, pattern = "dir_0.png")
-files_slr <- dir(in_path, pattern = "dir_slr.png")
-
-# loop through reserves to read in icons
-reserves <- unique(loc_dat$reserve)
-
-# set up the lists for the icons to go into
-# icons_0 <- list()
-# icons_slr <- list()
-
-file_paths_0 <- paste0(in_path, "/", reserves, "_dir_0.png")
-file_paths_slr <- paste0(in_path, "/", reserves, "_dir_slr.png")
-
-# for(i in seq_along(reserves)){
-#         res_in <- reserves[i]
-#         res_0 <- paste0(res_in, "_dir_0.png")
-#         res_slr <- paste0(res_in, "_dir_slr.png")
-#         
-#         
-#         # read in 0 icon
-#         if(res_0 %in% files_0){
-#                 icon_path <- here::here(in_path, res_0)
-#                 icons_0[[i]] <- makeIcon(iconUrl = icon_path,
-#                                          iconWidth = 30,
-#                                          iconHeight = 30)
-#                 
-#         } else {
-#                 icons_0[[i]] <- "you don't have a pie chart for this reserve"
-#         }
-#         
-#         
-#         # read in slr icon
-#         if(res_slr %in% files_slr){
-#                 icon_path <- here::here(in_path, res_slr)
-#                 icons_slr[[i]] <- makeIcon(iconUrl = icon_path,
-#                                          iconWidth = 30,
-#                                          iconHeight = 30)
-#                 
-#         } else {
-#                 icons_slr[[i]] <- "you don't have a pie chart for this reserve"
-#         }
-#         
-#         # name the piece of the list
-#         names(icons_0)[[i]] <- reserves[i]
-#         names(icons_slr)[[i]] <- reserves[i]
-# }
+# file names of pie charts for different purposes
+file_paths_0 <- dir(in_path, pattern = "dir_0.svg", full.names = TRUE)
+file_paths_slr <- dir(in_path, pattern = "dir_slr.svg", full.names = TRUE)
 
 
-# set up the color palette, for the legend
-to_color <- c("#c00000", "#f4a582", "#f0f0f0", "#92c5de", "#2f5597")  
+# set up the color palette and labels for the legend
+to_color <- c("#c00000", "#f4a582", "#fffacd", "#92c5de", "#2f5597") 
 names(to_color) <- c("Lower, CIs don't overlap", "Lower, CIs overlap", 
                      "Not enough information",
                      "Higher, CIs overlap", "Higher, CIs don't overlap")
 
-# make a map, using each icon for its associated location
 
+# make maps, using each icon for its associated location
 
-##############################
-# the help file for "icons" should be useful here
-# also that general part of the manual:
-# https://cran.r-project.org/web/packages/leaflet/leaflet.pdf
-##############################
-
-
+# compared to 0
 map_0 <- leaflet(loc_dat) %>% 
         addProviderTiles(leaflet::providers$Esri.WorldGrayCanvas) %>% 
         addMarkers(data = loc_dat,
@@ -88,12 +39,12 @@ map_0 <- leaflet(loc_dat) %>%
         addLegend(colors = to_color,
                   labels = names(to_color),
                   position = "bottomleft",
-                  opacity = 0.8,
+                  opacity = 0.9,
                   title = "Compared to 0")
 map_0
 
 
-
+# compared to sea level rise
 map_slr <- leaflet(loc_dat) %>% 
         addProviderTiles(leaflet::providers$Esri.WorldGrayCanvas) %>% 
         addMarkers(data = loc_dat,
@@ -107,7 +58,6 @@ map_slr <- leaflet(loc_dat) %>%
         addLegend(colors = to_color,
                   labels = names(to_color),
                   position = "bottomleft",
-                  opacity = 0.8,
+                  opacity = 0.9,
                   title = "Compared to long-term SLR")
 map_slr
-
